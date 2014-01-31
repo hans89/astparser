e2a9eb28e094de88e6ad6b9917cb1ce87759ec12
Hai Dang <dhhai.uns@gmail.com>
2014-01-31 23:36:34 +0900

    Add type reference test

--- TestSuit.java
+++ TestSuit.java
@@ -11,8 +11,12 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 
 /**
  * Tests for {@link astparser}.
@@ -21,8 +25,16 @@ import org.junit.runners.JUnit4;
  */
 @RunWith(JUnit4.class)
 public class TestSuit {
+	@Rule
+	public TestRule watcher = new TestWatcher() {
+	   protected void starting(Description description) {
+	      System.out.println("Starting test: " + description.getMethodName()
+	      	 + " .............");
+	   }
+	};
 
     @Test
+    @Ignore
     public void testDeclarationVisitor() {
 
 		HashMap<String,CompilationUnit> units = Parser.parse(
@@ -117,10 +129,54 @@ public class TestSuit {
 		}
     }
 
+
     @Test
     @Ignore
-    public void thisIsIgnored() {
+    public void testNodeTypesNameRef() {
+    	HashMap<String,CompilationUnit> units = Parser.parse(
+				FileUtils.getFiles("test-android/ast", 
+					new String[] {".java", ".JAVA"}),
+				new String[]{"lib/android/android-11.jar"},
+				new String[] { /* path that contain the source files*/
+					"test-android/ast"
+				});
+
+    	String[] androidClassKeys;
+
+		try {
+			androidClassKeys = 
+			FileUtils.getAllLines("android-ui/class.key");
+		} catch (java.io.IOException ex) {
+			System.out.println(ex);
+			return;
+		}
+
+		NameReferenceVisitor refVisitor = new NameReferenceVisitor(
+				androidClassKeys
+			);
+
+		for (Entry<String, CompilationUnit> e : units.entrySet()) {
+    		CompilationUnit u = e.getValue();
+			u.accept(refVisitor);
+		}
+
+		List<ReferenceInfo> refList = refVisitor.getNonEmpty();
+		List<ASTNode> nodes;
+
+		// 43: SimpleType
+		// http://grepcode.com/file/repository.grepcode.com/java/eclipse.org/3.6/org.eclipse.jdt/core/3.6.0/org/eclipse/jdt/core/dom/ASTNode.java
+
+		for (ReferenceInfo ref : refList) {
+			System.out.println(ref.getKey());
+			nodes = ref.getNodes();
+
+			for (ASTNode node: nodes) {
+				System.out.println("\tType: " + node.getNodeType() + 
+						" -- parent: " + node.getParent());
+			}
+		}
     }
+
 
     @Test
     public void testNodeTypesRef() {
@@ -140,6 +196,27 @@ public class TestSuit {
 		} catch (java.io.IOException ex) {
 			System.out.println(ex);
 			return;
+		}
+
+		TypeReferenceVisitor refVisitor = new TypeReferenceVisitor(
+				androidClassKeys
+			);
+
+		for (Entry<String, CompilationUnit> e : units.entrySet()) {
+    		CompilationUnit u = e.getValue();
+			u.accept(refVisitor);
+		}
+
+		List<ReferenceInfo> refList = refVisitor.getNonEmpty();
+		List<ASTNode> nodes;
+
+		for (ReferenceInfo ref : refList) {
+			System.out.println(ref.getKey());
+			nodes = ref.getNodes();
+
+			for (ASTNode node: nodes) {
+				System.out.println("\t-- " + node);
+			}
 		}
     }
 }
